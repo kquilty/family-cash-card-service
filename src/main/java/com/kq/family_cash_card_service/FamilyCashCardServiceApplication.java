4ec982f52d9bb39fb11a5a22bdade8f86ec5074c
Kyle Quilty <kquilty@gmail.com>
2025-10-29 13:55:50 -0400

    Raw output from "spring initializr"

---
+++ src/main/java/com/kq/family_cash_card_service/FamilyCashCardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kq.family_cash_card_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyCashCardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyCashCardServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kq/family_cash_card_service/FamilyCashCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kq.family_cash_card_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyCashCardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
